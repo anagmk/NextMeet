abb57e6521b2269b49fdf3809ac8767d77951f53
AnaG <you@example.commkanag40@gmail.com>
2026-09-12 00:16:52 +0530

    Port change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import { createServer } from "http";
 import app from "./app.js";
 import { setupSocket } from "./config/socketEvents.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const httpServer = createServer(app);
 setupSocket(httpServer);
 
